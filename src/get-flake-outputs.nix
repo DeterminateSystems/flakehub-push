d9b2f406b7815692be388a3f93f2291498ca483f
Eelco Dolstra <edolstra@gmail.com>
2023-08-08 20:24:03 +0200

    Don't fetch flake-schemas for flakes that have a schemas output

--- src/get-flake-outputs.nix
+++ src/get-flake-outputs.nix
@@ -22,7 +22,14 @@ in rec {
   defaultSchemas = (builtins.getFlake "https://api.flakehub.com/f/pinned/DeterminateSystems/flake-schemas/0.0.5%252Brev-92d8d7803fe5f3a3810a3cceb02fa6a4b65f15a6/0189c11e-9bb8-7bc5-a4fb-2df59ad36d55/source.tar.gz?narHash=sha256-Cv74iWkgDQeTiW3YKmvYC2RBoo4u133V73HZ%2BJnovVk%3D").schemas;
 
   # Ignore legacyPackages for now, since it's very big and throws uncatchable errors.
-  schemaOverrides.legacyPackages = defaultSchemas.legacyPackages // {
+  schemaOverrides.legacyPackages = {
+    version = 1;
+    doc = ''
+      The `legacyPackages` flake output is similar to `packages`, but it can be nested (i.e. contain attribute sets that contain more packages).
+      Since enumerating the packages in nested attribute sets is inefficient, `legacyPackages` should be avoided in favor of `packages`.
+
+      Note: the contents of `legacyPackages` are not shown in FlakeHub.
+    '';
     inventory = output: mkChildren {};
   };
 
