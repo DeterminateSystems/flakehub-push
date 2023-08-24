fdf8649ca0b233c1418ca9ddc6b4c9a78fd8242a
Cole Helbling <cole.helbling@determinate.systems>
2023-08-24 09:56:47 -0700

    cargo update, switch to std::io::IsTerminal

--- src/cli/instrumentation.rs
+++ src/cli/instrumentation.rs
@@ -1,6 +1,5 @@
-use atty::Stream;
 use color_eyre::eyre::WrapErr;
-use std::error::Error;
+use std::{error::Error, io::IsTerminal};
 use tracing_error::ErrorLayer;
 use tracing_subscriber::{
     filter::Directive, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter,
@@ -86,7 +85,7 @@ impl<'a> Instrumentation {
         S: tracing::Subscriber + for<'span> tracing_subscriber::registry::LookupSpan<'span>,
     {
         tracing_subscriber::fmt::Layer::new()
-            .with_ansi(atty::is(Stream::Stderr))
+            .with_ansi(std::io::stderr().is_terminal())
             .with_writer(std::io::stderr)
     }
 
@@ -95,7 +94,7 @@ impl<'a> Instrumentation {
         S: tracing::Subscriber + for<'span> tracing_subscriber::registry::LookupSpan<'span>,
     {
         tracing_subscriber::fmt::Layer::new()
-            .with_ansi(atty::is(Stream::Stderr))
+            .with_ansi(std::io::stderr().is_terminal())
             .with_writer(std::io::stderr)
             .pretty()
     }
@@ -105,7 +104,7 @@ impl<'a> Instrumentation {
         S: tracing::Subscriber + for<'span> tracing_subscriber::registry::LookupSpan<'span>,
     {
         tracing_subscriber::fmt::Layer::new()
-            .with_ansi(atty::is(Stream::Stderr))
+            .with_ansi(std::io::stderr().is_terminal())
             .with_writer(std::io::stderr)
             .json()
     }
@@ -115,7 +114,7 @@ impl<'a> Instrumentation {
         S: tracing::Subscriber + for<'span> tracing_subscriber::registry::LookupSpan<'span>,
     {
         tracing_subscriber::fmt::Layer::new()
-            .with_ansi(atty::is(Stream::Stderr))
+            .with_ansi(std::io::stderr().is_terminal())
             .with_writer(std::io::stderr)
             .compact()
             .without_time()
