735f4d0a38683cad3d57fc47af122cc6149d7f92
Graham Christensen <graham@grahamc.com>
2023-12-19 10:44:36 -0500

    fixup

--- flake.nix
+++ flake.nix
@@ -56,10 +56,8 @@
             CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
 
             buildInputs = lib.optionals (pkgs.stdenv.isDarwin) (with pkgs; [
-                #libiconv
-                #darwin.apple_sdk.frameworks.Security
-                darwin.apple_sdk.frameworks.SystemConfiguration
-              ]);
+              darwin.apple_sdk.frameworks.SystemConfiguration
+            ]);
           };
         });
 
