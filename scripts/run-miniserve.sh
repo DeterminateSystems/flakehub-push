da72bdf737f219e2dfb9f928e2f2d30b9de5bab7
Cole Mickens <cole.mickens@gmail.com>
2024-04-17 10:26:29 -0700

    refactor: remove testing scripts

--- scripts/run-miniserve.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -x
-set -euo pipefail
-
-DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd "${DIR}/.."
-
-miniserve -i :: --port 9898 ./target/x86_64-unknown-linux-musl/debug
