48a8b24c1e6edbc1b3341400dd6932a217f36126
Luc Perkins <lucperkins@gmail.com>
2024-04-21 14:22:34 -0300

    Add initial JS setup

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+// ts/index.ts
+function main() {
+  console.log("It works");
+}
+main();
+//# sourceMappingURL=index.js.map
---
+++ ts/index.ts
@@ -0,0 +1,5 @@
+function main(): void {
+  console.log("It works");
+}
+
+main();
